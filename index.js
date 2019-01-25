0b0127a28fafd5e8ae2e632b909450ca689d6086
mitchellhamilton <mitchell@hamil.town>
2019-01-25 15:14:03 +1000

    Add a try catch

--- index.js
+++ index.js
@@ -8,31 +8,33 @@ module.exports = babel => {
   return {
     visitor: {
       Program(programPath, state) {
-        let components = findExportedComponents(
-          programPath,
-          "flow",
-          state.file.filename
-        );
-        components.forEach(({ name, component }) => {
-          // TODO: handle when name is null
-          // it will only happen when it's the default export
-          // generate something like this
-          // export default (var someName = function() {}, someName.___types = theTypes, someName)
-          if (name !== null) {
-            programPath.node.body.push(
-              t.expressionStatement(
-                t.assignmentExpression(
-                  "=",
-                  t.memberExpression(
-                    t.identifier(name),
-                    t.identifier("___types")
-                  ),
-                  parseExpression(JSON.stringify(component))
+        try {
+          let components = findExportedComponents(
+            programPath,
+            "flow",
+            state.file.filename
+          );
+          components.forEach(({ name, component }) => {
+            // TODO: handle when name is null
+            // it will only happen when it's the default export
+            // generate something like this
+            // export default (var someName = function() {}, someName.___types = theTypes, someName)
+            if (name !== null) {
+              programPath.node.body.push(
+                t.expressionStatement(
+                  t.assignmentExpression(
+                    "=",
+                    t.memberExpression(
+                      t.identifier(name),
+                      t.identifier("___types")
+                    ),
+                    parseExpression(JSON.stringify(component))
+                  )
                 )
-              )
-            );
-          }
-        });
+              );
+            }
+          });
+        } catch (e) {}
       }
     }
   };
